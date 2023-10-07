04e69de9a57121d4742e6c6ee6369f1094bd7eef
Jagrati Choure <114644426+jgchoure19@users.noreply.github.com>
2023-10-07 18:13:06 +0530

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -140,8 +140,7 @@
        else  if(randomNumber >= 1/3 && randomNumber < 2/3 ){
          computerMove =  'paper';
        }
-       else{
-       if(randomNumber >= 2/3 && randomNumber < 1 )
+       else  if(randomNumber >= 2/3 && randomNumber < 1 ){
            computerMove =  'Scissors';
        }
 
